6aa88e91267586b18d41cbd66ada41d3b6f9f445
Allan Okoth <allanokoth695@gmail.com>
2022-12-16 11:39:01 +0200

    Update fairtrade_contract.py

--- fairtrade_contract.py
+++ fairtrade_contract.py
@@ -13,7 +13,6 @@ class Stage:
         return Seq([
             Assert(Txn.application_args.length() == Int(4)),
             Assert(Txn.note() == Bytes("fairtrade:uv1")),
-            Assert(Btoi(Txn.application_args[3]) > Int(0)),
             App.globalPut(self.Variables.title, Txn.application_args[0]),
             App.globalPut(self.Variables.summary, Txn.application_args[1]),
             App.globalPut(self.Variables.batch, Txn.application_args[2]),
